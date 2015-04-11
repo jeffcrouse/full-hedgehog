6bb71f82ee5cc3ff4d3690dcd20b3aceaea81794
Jeff Crouse <jeff@jeffish.com>
2015-04-11 18:45:25 +0000

    updated README

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ code for the Acer ferrofluid pool installation
 
 ##requirements
 - [pyOSC](https://pypi.python.org/pypi/pyOSC)
+- [oscP5](http://www.sojamo.de/libraries/oscP5)
--- pi/motorcontrol.py
+++ pi/motorcontrol.py
@@ -3,15 +3,13 @@
 from Adafruit_PWM_Servo_Driver import PWM
 import time, threading, OSC
 
-receive_address = '127.0.0.1', 7000
+receive_address = 'hedgehog.local', 7000
 s = OSC.OSCServer(receive_address)
 s.addDefaultHandlers()
 
 # Initialise the PWM device using the default address
 pwm = PWM(0x40, debug=True)
 
-servoMin = 150  # Min pulse length out of 4096
-servoMax = 600  # Max pulse length out of 4096
 
 def setServoPulse(channel, pulse):
 	pulseLength = 1000000                   # 1,000,000 us per second
@@ -23,18 +21,7 @@ def setServoPulse(channel, pulse):
 	pulse /= pulseLength
 	pwm.setPWM(channel, 0, pulse)
 
-
 pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
-# while (True):
-# 	for x in range(0, 9):
-# 		print "%d servoMin" % x
-# 		# Change speed of continuous servo on channel O
-# 		pwm.setPWM(x, 0, servoMin)
-# 		time.sleep(0.1)
-# 		print "%d servoMax" % x
-# 		pwm.setPWM(x, 0, servoMax)
-# 		time.sleep(0.1)
-
 
 def pwm_handler(addr, tags, stuff, source):
 	print "---"
@@ -43,9 +30,13 @@ def pwm_handler(addr, tags, stuff, source):
 	print "typetags %s" % tags
 	print "data %s" % stuff
 	print "---"
+	channel = stuff[0]
+	pulse = stuff[1]
+	pwm.setPWM(channel, 0, pulse )
 
 
 s.addMsgHandler("/pwm", pwm_handler)
+
 # just checking which handlers we have added
 print "Registered Callback-functions are :"
 for addr in s.getOSCAddressSpace():
@@ -68,3 +59,5 @@ except KeyboardInterrupt :
 	print "Waiting for Server-thread to finish"
 	st.join()
 	print "Done"
+
+
