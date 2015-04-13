7a1c33c3ec6cbd24f3f2fcb3ce5e34813fcc0b55
Jeff Crouse <jeff@jeffish.com>
2015-04-13 21:30:52 +0000

    updated min and max values

--- pi/motorcontrol.py
+++ pi/motorcontrol.py
@@ -8,31 +8,45 @@ s = OSC.OSCServer(receive_address)
 s.addDefaultHandlers()
 
 # Initialise the PWM device using the default address
-pwm = PWM(0x40, debug=True)
+pwm = PWM(0x40, debug=False)
 
+servoMin = 380  # Min pulse length out of 4096
+servoMax = 520  # Max pulse length out of 4096
 
-def setServoPulse(channel, pulse):
-	pulseLength = 1000000                   # 1,000,000 us per second
-	pulseLength /= 60                       # 60 Hz
-	print "%d us per period" % pulseLength
-	pulseLength /= 4096                     # 12 bits of resolution
-	print "%d us per bit" % pulseLength
-	pulse *= 1000
-	pulse /= pulseLength
-	pwm.setPWM(channel, 0, pulse)
+channel_map = [0, 1, 2, 3, 4, 5, 6, 7, 12]
+offset_map = [0, 0, 0, 0, 0, 40, 0, -15, 0]
+
+def ofMap(value, inputMin, inputMax, outputMin, outputMax, clamp=False):
+	if abs(inputMin - inputMax) < 1.0e-30:
+		print("ofMap: avoiding possible divide by zero, check inputMin and inputMax")
+		return outputMin
+	else:
+		outVal = float(float(value - inputMin) / float(inputMax - inputMin) * float(outputMax - outputMin) + outputMin)
+		if clamp:
+			if outputMax < outputMin:
+				if outVal < outputMax: 		outVal = outputMax
+				elif outVal > outputMin:  	outVal = outputMin
+			else:
+				if outVal > outputMax: 		outVal = outputMax
+				elif outVal < outputMin: 	outVal = outputMin
+		return outVal
+
 
 pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
 
 def pwm_handler(addr, tags, stuff, source):
-	print "---"
-	print "received new osc msg from %s" % OSC.getUrlStr(source)
-	print "with addr : %s" % addr
-	print "typetags %s" % tags
-	print "data %s" % stuff
-	print "---"
-	channel = stuff[0]
-	pulse = stuff[1]
-	pwm.setPWM(channel, 0, pulse )
+	i = stuff[0]
+	channel = channel_map[i]
+	value = ofMap(stuff[1], 0, 1, servoMin, servoMax, True) + offset_map[i]
+	# print "---"
+	# print "received new osc msg from %s" % OSC.getUrlStr(source)
+	# print "with addr : %s" % addr
+	# print "typetags %s" % tags
+	# #print "data %s" % stuff
+	# print "channel %s=%s" % (channel, value)
+	# print "---"
+
+	pwm.setPWM(channel, 0, int(value))
 
 
 s.addMsgHandler("/pwm", pwm_handler)
@@ -51,6 +65,7 @@ st.start()
 # Loop while threads are running.
 try :
 	while 1 :
+
 		time.sleep(10)
  
 except KeyboardInterrupt :
