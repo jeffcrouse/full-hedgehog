35158bec7cb4a8caab92e712d046a773a7f20449
Jeff Crouse <jeff@jeffish.com>
2015-04-14 20:11:29 +0000

    updated server script

--- pi/motorcontrol.py
+++ pi/motorcontrol.py
@@ -2,6 +2,7 @@
 
 from Adafruit_PWM_Servo_Driver import PWM
 import time, threading, OSC
+from ofMap import ofMap
 
 receive_address = 'hedgehog.local', 7000
 s = OSC.OSCServer(receive_address)
@@ -9,47 +10,26 @@ s.addDefaultHandlers()
 
 # Initialise the PWM device using the default address
 pwm = PWM(0x40, debug=False)
+pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+
 
 servoMin = 380  # Min pulse length out of 4096
 servoMax = 520  # Max pulse length out of 4096
-
-channel_map = [0, 1, 2, 3, 4, 5, 6, 7, 12]
-offset_map = [0, 0, 0, 0, 0, 40, 0, -15, 0]
-
-def ofMap(value, inputMin, inputMax, outputMin, outputMax, clamp=False):
-	if abs(inputMin - inputMax) < 1.0e-30:
-		print("ofMap: avoiding possible divide by zero, check inputMin and inputMax")
-		return outputMin
-	else:
-		outVal = float(float(value - inputMin) / float(inputMax - inputMin) * float(outputMax - outputMin) + outputMin)
-		if clamp:
-			if outputMax < outputMin:
-				if outVal < outputMax: 		outVal = outputMax
-				elif outVal > outputMin:  	outVal = outputMin
-			else:
-				if outVal > outputMax: 		outVal = outputMax
-				elif outVal < outputMin: 	outVal = outputMin
-		return outVal
-
-
-pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
-
 def pwm_handler(addr, tags, stuff, source):
-	i = stuff[0]
-	channel = channel_map[i]
-	value = ofMap(stuff[1], 0, 1, servoMin, servoMax, True) + offset_map[i]
+	channel = stuff[0]
+	value = ofMap(stuff[1], 0, 1, servoMin, servoMax, True)
 	# print "---"
 	# print "received new osc msg from %s" % OSC.getUrlStr(source)
 	# print "with addr : %s" % addr
 	# print "typetags %s" % tags
 	# #print "data %s" % stuff
-	# print "channel %s=%s" % (channel, value)
+	print "channel %s=%s" % (channel, value)
 	# print "---"
-
 	pwm.setPWM(channel, 0, int(value))
 
 
 s.addMsgHandler("/pwm", pwm_handler)
+
 
 # just checking which handlers we have added
 print "Registered Callback-functions are :"
@@ -65,9 +45,9 @@ st.start()
 # Loop while threads are running.
 try :
 	while 1 :
-
 		time.sleep(10)
- 
+
+
 except KeyboardInterrupt :
 	print "\nClosing OSCServer."
 	s.close()
